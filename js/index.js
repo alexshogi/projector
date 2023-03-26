b0d639ab30c71f8695a7a3c2b41c8687c4baaec6
alexshogi <alexahmetov7@gmail.com>
2023-03-26 14:45:06 +0300

    up

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const serviceButtons = document.querySelectorAll('[data-type="service-btn"]');
+const serviceLists = document.querySelectorAll('[data-type="service-list"]');
 const servicesBlock = document.querySelector('.services');
 
 
@@ -6,8 +7,13 @@ for (const serviceButton of serviceButtons) {
   serviceButton.addEventListener('click', function() {
     const value = serviceButton.dataset.value;
 
-    console.log(value);
+    serviceLists.forEach(list => {
+      list.style.display = 'none';
+    })
+
+    const list = document.querySelector(`[data-type="service-list"][data-value="${value}"]`);
 
     servicesBlock.style.backgroundImage = `url(./images/services/${value}.png)`;
+    list.style.display = 'unset';
   });
 }
